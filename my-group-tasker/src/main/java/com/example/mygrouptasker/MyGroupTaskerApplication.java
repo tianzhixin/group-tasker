f311825a86c04a27a7d0875f5038ebb6fd79b2ef
Frances Zhu <franceszhuyaqi@gmail.com>
2025-06-07 09:53:46 +1000

    Intial commit. Create the java web project.

---
+++ my-group-tasker/src/main/java/com/example/mygrouptasker/MyGroupTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygrouptasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGroupTaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGroupTaskerApplication.class, args);
+    }
+
+}
---
+++ my-group-tasker/src/test/java/com/example/mygrouptasker/MyGroupTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygrouptasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGroupTaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
