d4da9b9cb6792d956dbc3591fe86671c09b2aa14
Frances Zhu <franceszhuyaqi@gmail.com>
2025-07-27 10:12:36 +1000

    Complete the oas design for tasks related paths

---
+++ my-group-tasker/webapp/src/main/java/com/example/tasker/webapp/api/controller/TaskController.java
@@ -0,0 +1,21 @@
+package com.example.tasker.webapp.api.controller;
+
+import com.example.tasker.models.model.TaskPage;
+import com.example.tasker.skeleton.api.TasksApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/group-tasker")
+public class TaskController implements TasksApi {
+    private static final Logger LOG = LoggerFactory.getLogger(GroupController.class);
+
+    @Override
+    public ResponseEntity<TaskPage> getTasks(Integer size, Integer page){
+        return new ResponseEntity<>(new TaskPage(), HttpStatus.OK);
+    }
+}
